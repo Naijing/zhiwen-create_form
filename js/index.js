e192ce30aadaf96b7019e3d8f234c5790eb5a4bf
jinjin <wangxiao8808@hotmail.com>
2017-02-06 07:43:49 +0100

    zhiwen 登录界面

---
+++ js/index.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+
+	$('#search_button').button({
+		icons:{
+			primary: 'ui-icon-search',
+		},
+	});
+
+	$('#reg').dialog({
+		autoOpen:true,
+		modal:true,
+		resizable:false,
+		width:320,
+		height:340,
+		buttons: {
+			'提交':function(){}
+		}
+	});
+
+	$('#reg').buttonset();
+	$('#date').datepicker({
+		showOn: 'button',
+		//showOn: 'both',
+		//buttonText: 'D',
+		buttonImage:'img/calendar.gif',
+		buttonImageOnly:true,
+	});
+	$('#reg input[title]').tooltip({
+		show:false,
+		hide:false,
+		position:{
+			my:'left-center',
+			at:'right+5 center'
+		},
+	});
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
